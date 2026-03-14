0dade2d66229f8bb7b3d8bb138aef4dc3817a301
firebrsa <firefoxbursa@outlook.com.tr>
2026-03-15 00:40:53 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T21:40:47.544Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-14T21:40:53.533Z
+Commit: Add export functionality for reports
