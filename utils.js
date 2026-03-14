db9b11fd773306643b96362cd106e4092814fede
firebrsa <firefoxbursa@outlook.com.tr>
2026-03-15 00:40:55 +0300

    Add dark mode support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T21:40:51.476Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-14T21:40:55.597Z
+Commit: Add dark mode support
