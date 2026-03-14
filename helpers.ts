c6aa0aa84e0324501d11ad17a06d5d4cada99644
firebrsa <firefoxbursa@outlook.com.tr>
2026-03-15 01:18:53 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:18:40.322Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-14T22:18:53.307Z
+Commit: Add dark mode support
